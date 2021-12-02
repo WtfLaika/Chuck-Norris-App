0e15db785540ebecc025f4c7911db6ed9e1401df
Denys Shmahailo <omgdoni@gmail.com>
2021-12-02 16:19:12 +0200

    docker

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,8 @@
-import axios from "axios";
 import React from "react";
 import { useEffect } from "react";
-import { Provider } from "react-redux";
 import { UserActionTypes } from "../redux/type";
 import JokesService from "../services/Jokes.service";
 import { useAppSelector } from "./../app/hooks";
-import { store } from "./../index";
 
 const { REACT_APP_JOKES_URL, REACT_APP_RANDOM } = process.env;
 
